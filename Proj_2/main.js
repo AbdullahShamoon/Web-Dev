d26fb5f9453a61adaa72766d34f776013fb33ba5
Abdullah <abdullahshamoon24@gmail.com>
2024-10-21 16:48:23 +0530

    Added more code :)

--- Proj_2/main.js
+++ Proj_2/main.js
@@ -6,14 +6,38 @@ mongoose.connect('mongodb://127.0.0.1:27017/company');
 
 const port = 3000
 
+//Fuction to get random data
+const getRandom = (arr)=>{
+  return arr[Math.floor(Math.random()*(arr.length-1))]
+}
+
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('index', {});
+  res.render('index', {});
 })
 
-app.get('/generate', (req, res) => {
-  res.send('Hello World!')
+app.get('/generate', async(req, res) => {
+  
+  //Delete all data
+  await Employee.deleteMany({});
+
+  //Random dataset
+  let randomName=['Abdullah','Shamoon','Shamim','Hassan','Rizwan']
+  let randomlanguage=['Python','Java','C++','C#','JavaScript']
+  let randomcity=['Karachi','Lahore','Islamabad','Peshawar','Quetta']
+  
+  for (let i = 0; i < 10; i++) {
+    const employee = new Employee({
+      name: getRandom(randomName),
+      salary: Math.floor(Math.random() * 100000),
+      language: getRandom(randomlanguage),
+      city: getRandom(randomcity),
+      isManager: (Math.random() < 0.5)?true:false
+    })
+    employee.save();
+    console.log(employee)
+  }
 })
 
 app.listen(port, () => {
